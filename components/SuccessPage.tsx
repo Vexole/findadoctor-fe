5a21ea496d889085ff93ee838ba61973cf2efe81
Bhupesh Shrestha <bhupesh.stha@gmail.com>
2023-08-01 16:54:05 -0400

    Making it generic

--- components/SuccessPage.tsx
+++ components/SuccessPage.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
 
-export const SuccessPage = ({
+const SuccessPage = ({
   successTitle,
   successMessage,
-  button1Text,
-  button2Text,
   handleButton1Click,
-  handleButton2Click
+  button1Text,
+  handleButton2Click,
+  button2Text,
 }: {
   successTitle: string;
   successMessage: string;
@@ -16,15 +16,27 @@ export const SuccessPage = ({
   handleButton2Click?: any
 }) => {
   return (
-    <div>
-      <h2>{successTitle}</h2>
-      <p>{successMessage}</p>
-      <div>
-        {handleButton1Click != null &&
-          <button onClick={handleButton1Click}>{button1Text}</button>}
-        {handleButton2Click != null &&
-          <button onClick={handleButton2Click}>{button2Text}</button>}
-      </div>
-    </div>
+    <Box p={4} maxW="400px" mx="auto" mt={10}>
+      <Heading as="h2" size="lg" mb={4} textAlign="center" color="green.600">
+        {successTitle}
+      </Heading>
+      <Text fontSize="lg" textAlign="center" color="gray.600">
+        {successMessage}
+      </Text>
+      <Box mt={6} display="flex" justifyContent="center">
+        {handleButton1Click != null && (
+          <Button colorScheme="teal" onClick={handleButton1Click} mr={2}>
+            {button1Text}
+          </Button>
+        )}
+        {handleButton2Click != null && (
+          <Button colorScheme="blue" onClick={handleButton2Click}>
+            {button2Text}
+          </Button>
+        )}
+      </Box>
+    </Box>
   );
 };
+
+export default SuccessPage;
