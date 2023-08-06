f3252ec46b223c9b5a7f23429bbae35c7e7032b7
Bhupesh Shrestha <bhupesh.stha@gmail.com>
2023-08-06 15:15:54 -0400

    Glich fixes

--- hooks/useDoctorAppointmentsQuery.tsx
+++ hooks/useDoctorAppointmentsQuery.tsx
@@ -1,8 +1,8 @@
 import { getDoctorAppointments } from "@/api/appointment";
 import { useQuery } from "@tanstack/react-query";
 
-const authenticatedUser = localStorage.user ? JSON.parse(localStorage.user) : {};
-const userId = authenticatedUser?.userId;
-
-export const useDoctorAppointmentsQuery = () =>
-    useQuery(["appointments", userId], () => getDoctorAppointments(userId));
+export const useDoctorAppointmentsQuery = () => {
+    const authenticatedUser = localStorage.user ? JSON.parse(localStorage.user) : {};
+    const userId = authenticatedUser.userId;
+    return useQuery(["appointments", userId], () => getDoctorAppointments(userId));
+}
